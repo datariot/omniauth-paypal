9db0af85aff31c869990e81a9c6647378b7d1e84
Logan Hasson <logan.hasson@gmail.com>
2014-05-15 23:33:16 -0400

    Update tests to use newer RSpec 'expect' syntax

--- spec/omniauth/strategies/paypal_spec.rb
+++ spec/omniauth/strategies/paypal_spec.rb
@@ -10,40 +10,40 @@ describe OmniAuth::Strategies::PayPal do
 
   describe '#client' do
     it 'has correct PayPal site' do
-      subject.client.site.should eq('https://api.paypal.com')
+      expect(subject.client.site).to eq('https://api.paypal.com')
     end
 
     it 'has correct PayPal sandbox site' do
       @options = { :sandbox => true }
       subject.setup_phase
-      subject.client.site.should eq('https://api.sandbox.paypal.com')
+      expect(subject.client.site).to eq('https://api.sandbox.paypal.com')
     end
 
     it 'has correct authorize url' do
-      subject.client.options[:authorize_url].should eq('https://www.paypal.com/webapps/auth/protocol/openidconnect/v1/authorize')
+      expect(subject.client.options[:authorize_url]).to eq('https://www.paypal.com/webapps/auth/protocol/openidconnect/v1/authorize')
     end
 
     it 'has correct sandbox authorize url' do
       @options = { :sandbox => true }
       subject.setup_phase
-      subject.client.options[:authorize_url].should eq('https://www.sandbox.paypal.com/webapps/auth/protocol/openidconnect/v1/authorize')
+      expect(subject.client.options[:authorize_url]).to eq('https://www.sandbox.paypal.com/webapps/auth/protocol/openidconnect/v1/authorize')
     end
 
     it 'has correct token url' do
-      subject.client.options[:token_url].should eq('/v1/identity/openidconnect/tokenservice')
+      expect(subject.client.options[:token_url]).to eq('/v1/identity/openidconnect/tokenservice')
     end
 
     it 'runs the setup block if passed one' do
       counter = ''
       @options = { :setup => Proc.new { |env| counter = 'ok' } }
       subject.setup_phase
-      counter.should eq "ok"
+      expect(counter).to eq("ok")
     end
   end
 
   describe '#callback_path' do
     it "has the correct callback path" do
-      subject.callback_path.should eq('/auth/paypal/callback')
+      expect(subject.callback_path).to eq('/auth/paypal/callback')
     end
   end
 

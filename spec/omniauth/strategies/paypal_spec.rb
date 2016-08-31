105719eee681995aecdb97e6bc0e788829549198
Cristian Stănescu <cristianstanescu@gmail.com>
2016-08-31 17:00:58 +0300

    Comply with Omniauth Auth Hash Schema
    Normalize the hash information to comply with Omniauth strategy
    developemnt recommendations:
    
    - remove key/value pairs from #info that are missing from the
    Omniauth Auth Hash Schema.
    
    - add all user information returned from Paypal as a value for the
    'raw_info' key in #extra.
    
    Add tests for #info, #extra and also for #uid.

--- spec/omniauth/strategies/paypal_spec.rb
+++ spec/omniauth/strategies/paypal_spec.rb
@@ -6,6 +6,36 @@ describe OmniAuth::Strategies::PayPal do
     OmniAuth::Strategies::PayPal.new(nil, @options || {})
   end
 
+  let(:user_info_hash) {
+    {
+      'payer_id' => '6ZGXTKGQ3L35N',
+      'family_name' => 'Harkonnen',
+      'verified' => 'true',
+      'name' => 'Mikka Harkonnen',
+      'account_type' => 'PERSONAL',
+      'given_name' => 'Mikka',
+      'user_id' =>
+        'https://www.paypal.com/webapps/auth/identity/user/' \
+        'K43VMDJ6KaRJgMVUFRGT3hqpdnhg1tDYLmlPgxl1HRE',
+      'address' => {
+        'postal_code' => 'W12 4LQ',
+        'locality' => 'Wolverhampton',
+        'region' => 'West Midlands',
+        'country' => 'GB',
+        'street_address' => '1 Main Terrace'
+      },
+      'verified_account' => 'true',
+      'language' => 'en_GB',
+      'zoneinfo' => 'America/Los_Angeles',
+      'locale' => 'en_GB',
+      'phone_number' => '0356739226',
+      'account_creation_date' => '2016-08-30',
+      'email' => 'test2@login.com',
+      'age_range' => '36-40',
+      'birthday' => '1975-10-10'
+    }
+  }
+
   it_should_behave_like 'an oauth2 strategy'
 
   describe '#client' do
@@ -37,14 +67,72 @@ describe OmniAuth::Strategies::PayPal do
       counter = ''
       @options = { :setup => Proc.new { |env| counter = 'ok' } }
       subject.setup_phase
-      expect(counter).to eq("ok")
+      expect(counter).to eq('ok')
     end
   end
 
   describe '#callback_path' do
-    it "has the correct callback path" do
+    it 'has the correct callback path' do
       expect(subject.callback_path).to eq('/auth/paypal/callback')
     end
   end
 
+  describe '#uid' do
+    before do
+      allow(subject).to receive(:raw_info).and_return(user_info_hash)
+    end
+
+    it 'returns the final part of the PayPal user_id URL' do
+      expect(subject.uid).to eql('K43VMDJ6KaRJgMVUFRGT3hqpdnhg1tDYLmlPgxl1HRE')
+    end
+  end
+
+  describe '#info' do
+    before do
+      allow(subject).to receive(:raw_info).and_return(user_info_hash)
+    end
+
+    it 'includes the name' do
+      expect(subject.info[:name]).to eq(user_info_hash['name'])
+    end
+
+    it 'includes the first_name' do
+      expect(subject.info[:first_name]).to eq(user_info_hash['given_name'])
+    end
+
+    it 'includes the last_name' do
+      expect(subject.info[:last_name]).to eq(user_info_hash['family_name'])
+    end
+
+    it 'includes the email' do
+      expect(subject.info[:email]).to eq(user_info_hash['email'])
+    end
+
+    it 'includes the location as city and state when address exists' do
+      location = [
+        user_info_hash['address']['locality'],
+        user_info_hash['address']['region']
+      ].join(', ')
+      expect(subject.info[:location]).to eq(location)
+    end
+
+    it 'does not include the location when address does not exist' do
+      user_info_hash.delete('address')
+      expect(subject.info[:location]).to be_nil
+    end
+
+    it 'includes the phone' do
+      expect(subject.info[:phone]).to eq(user_info_hash['phone_number'])
+    end
+  end
+
+  describe '#extra' do
+    before do
+      allow(subject).to receive(:raw_info).and_return(user_info_hash)
+    end
+
+    it 'contains raw info as a hash value of :raw_info' do
+      expect(subject.extra).to eq({ 'raw_info' => user_info_hash })
+    end
+  end
 end
